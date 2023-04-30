23f9655b84ca31edf4ef0471d92dc8cbcd1b1b8d
Akash <akashtale910@gmail.com>
2023-05-01 03:17:16 +0530

    News component added

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import './App.css'
 import React,{useState}from 'react'
 import NavBar from './components/NavBar'
 import News from './components/News'
-import {BrowserRouter as Router,Switch,Routes,Link} from "rect-router-dom"
+import {BrowserRouter as Router,Switch,Routes,Link,Route} from "react-router-dom"
 
 const App=()=>{
   const pageSize=5;
-  const apikey=ssd;
-  const [progress,setProgess]=useState(0);
+  const apiKey="ss";
+  const [progress,setProgress]=useState(0);
 
   return (
-    <div>
+    <div> 
       <Router>
         <NavBar/>
         <Routes>
-        <Route exact path="/" element={<News key="general" pageSize={5} country="in" category="general"/>}/>
-        <Route exact path="/business" element ={<News key="business" pageSize={5} country="in" category="business"/>}/>
-        <Route exact path="/entertainment" element={<News key="health" pageSize={5} country="in" category="health"/>}/>
-        <Route exact path="/science" element={<News key="science" pageSize={5} country="in" category="science"/>}/>
-        <Route exact path="/sports" element={<News key="sports" pageSize={5} country="in" category="sports"/>}/>
-        <Route exact path="/technology"element={<News key="technology" pageSize={5} country="in" category="technology"/>}/>
+        <Route exact path="/" element={<News key="general" setProgress={setProgress} apiKey={apiKey}  pageSize={5} country="in" category="general"/>}/>
+        <Route exact path="/business" element ={<News key="business" setProgress={setProgress} apiKey={apiKey}  pageSize={5} country="in" category="business"/>}/>
+        <Route exact path="/entertainment" element={<News key="health" setProgress={setProgress} apiKey={apiKey}  pageSize={5} country="in" category="health"/>}/>
+        <Route exact path="/science" element={<News key="science" setProgress={setProgress} apiKey={apiKey}  pageSize={5} country="in" category="science"/>}/>
+        <Route exact path="/sports" element={<News key="sports" setProgress={setProgress} apiKey={apiKey}  pageSize={5} country="in" category="sports"/>}/>
+        <Route exact path="/technology"element={<News key="technology" setProgress={setProgress} apiKey={apiKey}  pageSize={5} country="in" category="technology"/>}/>
         </Routes>
       </Router>
     </div>
   )
 }
-
+export default App;
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <Link class="navbar-brand" to="/">
+            NewsMonkey
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg=0">
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" To="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/business">
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/entertainment">
+                  Entertainment
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/general">
+                  General
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/health">
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science">
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports">
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/technology">
+                  Technology
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default NavBar
